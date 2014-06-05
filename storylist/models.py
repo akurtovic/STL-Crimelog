014ce6e9e8e1edb1e88b2959ea58edface914fb2
Amir Kurtovic <amirkurtovic@gmail.com>
2014-06-05 17:03:19 -0500

    Minor edit to model

--- storylist/models.py
+++ storylist/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Story(models.Model):
     headline = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
+    url = models.CharField(max_length=250)
     source = models.CharField(max_length=50)
     added = models.DateTimeField()
+
+    def __unicode__(self):
+    	return u'%s: %s' % (self.source, self.headline)
