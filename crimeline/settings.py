af94136c17a30bca499dbb4c0dc344a98bb56b38
Amir Kurtovic <amirkurtovic@gmail.com>
2014-06-24 19:26:03 -0500

    Edits to templates

--- crimeline/settings.py
+++ crimeline/settings.py
@@ -13,11 +13,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-TEMPLATE_PATH = '/home/amirkurtovic/crimeline/storylist'
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'storylist')
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     TEMPLATE_PATH,
 )
 
